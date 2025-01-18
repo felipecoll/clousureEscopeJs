edf9bae6aa69192d6b317bb226576d9f8f1c18b3
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-18 08:21:31 -0300

    Clase 4 - Asignacion y declaracion

---
+++ clase4-asigReasigDeclarac.js
@@ -0,0 +1,36 @@
+var firstName // undefined
+firstName = 'Felipe'
+
+console.log(firstName)
+
+var lastName = 'David' //Declarar asignar
+lastName = 'Ana' // Reasignar
+console.log(lastName)
+
+var secondName = 'David' // Declarando asignando
+var secondName = 'Ana' // Re asignando
+
+console.log(secondName)
+
+// Let
+
+let fruit = 'Apple'
+fruit = 'Kiwi'
+
+let fruit = 'Banana'
+
+
+// Const 
+
+const animal = 'dog' // declara y asigna
+animal = 'cat' // reasignado
+const animal = 'snake'
+console.log(animal)
+
+
+const vehiculos = []
+vehiculos.push('Camioneta')
+console.log(vehiculos)
+
+vehiculos.pop()
+console.log(vehiculos)
