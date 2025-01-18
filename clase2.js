addd6d26436c95381e16ddfd7e2fd0837c6531c9
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-18 07:55:41 -0300

    Clase 2 - Function scope

--- clase2.js
+++ clase2.js
@@ -1,1 +1,13 @@
 // Function Scope
+
+function greeting(){
+    let username = 'Felipe'
+    console.log(username)
+
+    if (username === 'Felipe') {
+        console.log(`Hello ${username}!`)
+    }
+}
+
+greeting()
+console.log(username)
